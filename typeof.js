723f970bbf0712dfc0dec1d801a46dfb24112477
Muhammad Arslan <marslan.be20igis@student.nust.edu.pk>
2023-04-11 23:02:04 +0500

    Data Types in JS

---
+++ typeof.js
@@ -0,0 +1,11 @@
+// var str = "Arslan";
+// var num = 12.6;
+// var arr = ['arslan', 'hayat', 'essa'];
+// var obj = {
+//     name: "Arslan",
+//     age : 21,
+//     class: "BSCS"
+// }
+// var bool = true;
+
+console.log(typeof(bool));
