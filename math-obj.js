34536741fc18458a376ea136b9a29631320589f7
Muhammad Arslan <marslan.be20igis@student.nust.edu.pk>
2023-04-11 23:15:19 +0500

    Math objects

---
+++ math-obj.js
@@ -0,0 +1,116 @@
+// random and ceil, floor methods
+// Math.random() method outputs the number between 0 and 0.9999999999999999
+// Math.ceil() method outputs the number rounded up to the nearest integer
+// Math.floor() method outputs the number rounded down to the nearest integer
+var random = Math.random();
+random = random * 6;
+var ceil = Math.ceil(random);
+var floor = Math.floor(random);
+console.log("The upper number" ,ceil);
+console.log("The lower number" ,floor);
+
+
+
+// //round method
+// //round method returns the number rounded to the nearest integer
+// var random = Math.random();
+// random = random * 6;
+// var round = Math.round(random);
+// console.log(round);
+
+
+
+// //abs method returns the absolute value of the number
+// var abs = Math.abs(-105)
+// console.log(abs);
+
+
+// //pow method returns the value of the number raised to the power of the number
+// var pow = Math.pow(4, 2);
+// console.log(pow);
+
+// //sqrt method returns the square root of the number
+// var sqrt = Math.sqrt(16);
+// console.log(sqrt);
+
+// //max method returns the largest number
+// var max = Math.max(5, 10, 15, 20);
+// console.log("Max number is: " ,max);
+
+// //min method returns the smallest number
+// var min = Math.min(5, 10, 15, 20);
+// console.log("Min number is: ", min);
+
+
+// //log method returns the natural logarithm of the number
+// var log = Math.log(2.718);
+// console.log(log);
+
+
+// //exp method returns the value of e raised to the power of the number
+// var exp = Math.exp(2);
+// console.log(exp);
+
+
+// //sin method returns the sine of the number
+// var sin = Math.sin(45);
+// console.log(sin);
+
+
+
+// //length property returns the length of the string
+// var str = "Ali Azam";
+// console.log(str.length);
+
+
+// //charAt method returns the character at the specified index
+// var str = "Muhammad";
+// var charAt = str.charAt(2);
+// console.log(charAt);
+
+
+// charCodeAt method returns the Unicode of the character at the specified index
+// var str = "Muhammad";
+// var charCodeAt = str.charCodeAt(4);
+// console.log(charCodeAt);
+
+
+// //concat method returns the string by concatenating the specified string
+// var str = "Hello";
+// var concat = str.concat(" Arslan!");
+// console.log(concat);
+
+
+// //indexOf method returns the index of the first occurrence of the specified value
+// var indexOf = str.indexOf("o");
+// console.log(indexOf);
+
+
+// //lastIndexOf method returns the index of the last occurrence of the specified value
+// var lastIndexOf = str.lastIndexOf("o");
+// console.log(lastIndexOf);
+
+
+// //match method returns an array of all matches of the specified value
+// var str = "Hello World";
+// var match = str.match(/lo/g);
+// console.log(match);
+
+
+
+// //replace method returns the 
+// var replace = str.replace("Hello World", "Hi Arslan");console.log(replace);
+
+// //search method
+// var search = str.search("World");
+// console.log("The string lies at the index", search);
+
+
+// //slice method
+// var slice = str.slice(0, 8);
+// console.log(slice);
+
+
+// //split method
+// var split = str.split("");
+// console.log(split);
