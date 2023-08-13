7f8b8e420271153c7e01d218bd9e15a572454b3d
Muhammad Arslan <98019141+arslansalyana@users.noreply.github.com>
2023-08-13 14:06:33 +0500

    added pig Latin Converter in JavaScript

---
+++ pigLatinConverter.js
@@ -0,0 +1,31 @@
+// const sentence = prompt("Enter the Sentence!");
+// function pigLatinConverter (sentence){
+//    const arr = sentence.split(' ');
+//    let pigLatinArray = [];
+//    for (let i = 0; i < arr.length; i++){
+//       const char1 = arr[i].substring(0,1);
+//       const restOfTheChars = arr[i].substring(1,arr[i].length);
+//       const arrFinal = restOfTheChars+char1+"ay";
+//       pigLatinArray.push(arrFinal);
+//    }
+//    const pigLatinSentence = pigLatinArray.join(" ");
+//    console.log(pigLatinSentence);
+// }
+// pigLatinConverter(sentence);
+
+const sentence = prompt("Enter the Sentence!");
+
+function pigLatinConverter (sentence){
+   const arr = sentence.split(' ');
+
+   const pigLatinArray = arr.map((singleWord) => {
+      const char1 = singleWord[0];
+      const restOfTheChars = singleWord.slice(1,singleWord.length);
+      return restOfTheChars + char1 + "ay";
+   })
+   
+   const pigLatinSentence = pigLatinArray.join(" ");
+   console.log(pigLatinSentence);
+}
+
+pigLatinConverter(sentence);
