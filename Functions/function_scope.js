d956c78b988aa7f3656ebf2af1fa04dc7a14aac9
Muhammad Arslan <98019141+arslansalyana@users.noreply.github.com>
2023-09-20 20:09:49 +0000

    function scope in depth

--- Functions/function_scope.js
+++ Functions/function_scope.js
@@ -21,3 +21,55 @@ function outer(){
 // console.log(outer());
 //inner functions can access the variables defined in outer function, but outer cant.
 
+
+// The following variables are defined in the global scope
+const num1 = 20;
+const num2 = 3;
+const name = "Arslan";
+
+function multiply() {
+    return num1 * num2;
+}
+//so function defined inside the global scope can access the variables defined inside the global scope.
+// console.log(multiply());
+
+const myName = "Salyana"
+
+function getScore(){
+    const num1 = 10;
+    const num2 = 3;
+
+    function add(){
+        return `${myName} scored ${num1 + num2}`;
+    }
+    return add();
+}
+// console.log(getScore());
+// A function defined inside another function can also access all variables defined in 
+// its parent function, and any other variables to which the parent function has access.
+
+function fun1(){
+    const n1 = 1;
+    const firstName = "Muhammad";
+
+    function fun2(){
+        const n2 = 2;
+        const middleName = "Arslan";
+
+        function fun3(){
+            const n3 = 3;
+            const lastName = "Salyana";
+            function fun4(){
+                const n4 = 4;
+                const cast = "sial";
+                return `${firstName +" "+middleName +" "+lastName + " " + cast} scored ${n1 + n2 + n3 + n4}`
+            }
+            return fun4();
+        }
+        return fun3();
+    }
+    return fun2();
+}
+//this example illustrates that, any nested function can access all the varibales defied above it, like its
+// parent function variables and then parents of those and so on.
+// console.log(fun1());
