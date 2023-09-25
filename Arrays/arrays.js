df86beb820bc12862d007d8b05704ce3d43de0c0
Muhammad Arslan <98019141+arslansalyana@users.noreply.github.com>
2023-09-25 22:17:47 +0000

    added array properties in comments

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,4 +1,26 @@
-//Just a random easy example.
+//Arrays: In JavaScript, arrays aren't primitives but are instead Array objects.
+// Arrays are resizeable, mix of different data types.
+// Arrays are zero-indexed. First element is at index 0, array[0];
+//last element of the array, (array.length-1)
+
+
+//Very Imp: JavaScript array-copy operations create shallow copies. 
+//(All standard built-in copy operations with any JavaScript objects 
+//create shallow copies, rather than deep copies).
+
+//Shallow Copy: A shallow copy of an object is a copy whose properties share the same references
+// (point to the same underlying values) as those of the source object from which the copy was made.
+//  As a result, when you change either the source or the copy, you may also cause the other object to change too.
+
+
+// A deep copy of an object is a copy whose properties do not share the same references 
+// (point to the same underlying values) as those of the source object from which the copy 
+// was made. As a result, when you change either the source or the copy, you can be assured
+//  you're not causing the other object to change too. That behavior contrasts with the 
+//  behavior of a shallow copy, in which changes to nested properties in the source or the
+//   copy may cause the other object to change too.
+
+
 const bands = ["B1", "B2", "B3", "B4", "B5", "B6", "B7"];
 function listarrayitems(arr){
     for (i = 0; i < arr.length; i++){
